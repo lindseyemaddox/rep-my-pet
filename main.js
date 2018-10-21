44377508edfd11b15b047012e515093c16d041d2
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-21 10:03:22 -0500

    created object

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const pet = {
+  name: "Stella",
+  species: "Boxer",
+  nicknames: {
+    full: "Stella Beef the Biscuit Thief",
+    short: "Beef"
+  },
+  age: 6
+};
